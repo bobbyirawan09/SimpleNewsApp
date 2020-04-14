b2ffa67b5518afabb02c382d618e300b1a147bcf
Bobby irawan <irawanbobby09@gmail.com>
2020-04-14 23:14:27 +0700

    [bobbyirawan09] make image rounded, use coil and data binding to show image

--- app/src/main/java/bobby/irawan/simplenewsapp/utils/DataBindingUtils.kt
+++ app/src/main/java/bobby/irawan/simplenewsapp/utils/DataBindingUtils.kt
@@ -3,13 +3,17 @@ package bobby.irawan.simplenewsapp.utils
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import coil.api.load
+import coil.transform.CircleCropTransformation
 
 object DataBindingUtils {
 
     @BindingAdapter("bind:setImageCoil")
     @JvmStatic
     fun setImageCoil(imageView: ImageView, urlImage: String) {
-
+        imageView.load(urlImage) {
+            crossfade(true)
+        }
     }
 
     @BindingAdapter("bind:setPublishDate")
